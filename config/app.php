897a754d34697b9c535c834aaf71914f296061c9
mhixv <mhi.henri@yahoo.com>
2016-06-16 05:28:11 +0700

    library added

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Collective\Html\HtmlServiceProvider::class,
 
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
 
     ],
 
@@ -212,7 +213,7 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
 
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-
+        'PDF' => Barryvdh\DomPDF\Facade::class,
     ],
 
 ];
