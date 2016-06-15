f7df3bd2a1f6dd0e7b905ed6c6bf7f8206e597fd
Indra Lukmana <indra.lukmana.il@gmail.com>
2016-06-16 00:27:48 +0700

    Create Seeder for Kabupaten and Provinsi
    -Create Database Seeder for Kabupaten and Provinsi
    -Add the seeder into DatabaseSeeder
    -The Provinsi must be called before the Kabupaten

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(ProvinsiTableSeeder::class);
+        $this->call(KabupatenTableSeeder::class);
     }
 }
---
+++ database/seeds/KabupatenTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class KabupatenTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('kabupaten')->delete();
+
+        $faker = Faker\Factory::create('id_ID');
+
+        foreach(range(1, 30) as $index){
+            $provinsiRandom = DB::table('provinsi')->first();
+
+            DB::table('kabupaten')->insert([
+                'nama_kabupaten' => $faker->city,
+                'provinsi_id_provinsi' => $provinsiRandom->id_provinsi
+            ]);
+        }
+    }
+}
