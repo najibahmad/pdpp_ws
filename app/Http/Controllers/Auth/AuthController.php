eb8eed85c1fc616b8319f2dad1bcc53e4e1e9cdf
indi60 <agung.smarts@gmail.com>
2016-06-15 07:03:55 +0700

    before get latest

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -84,17 +84,17 @@ class AuthController extends Controller
         return redirect('/');
     }
 
-    public function showRegistrationForm() {
-        return redirect('/');
-    }
+    // public function showRegistrationForm() {
+    //     return redirect('/');
+    // }
 
-    public function showLoginForm()
-    {
-        if(!session()->has('from')){
-            session()->put('from', url()->previous());
-        }
-        return view('auth.login');
-    }
+    // public function showLoginForm()
+    // {
+    //     if(!session()->has('from')){
+    //         session()->put('from', url()->previous());
+    //     }
+    //     return view('auth.login');
+    // }
 
     public function authenticated($request,$user)
     {
